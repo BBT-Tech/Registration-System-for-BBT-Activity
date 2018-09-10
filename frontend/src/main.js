0f57fdb22e74dc76e1333b13a7ba9d32ae030a7f
Daly <1315190351@qq.com>
2018-09-10 08:49:38 +0800

    console.log added

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import './console.log'
 import Vue from 'vue'
 import router from './router'
 import VueResource from 'vue-resource'
